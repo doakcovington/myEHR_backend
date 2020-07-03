d22ce5a1db4759c0cbefd0a133464321fd4a9cc7
doakcovington <doakcovington@protonmail.com>
2020-07-02 19:25:40 -0700

    Built out records controller

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -1,6 +1,23 @@
 class Api::V1::RecordsController < ApplicationController
     def index
-        @records = Record.all
-        render json: @records
+        records = Record.all
+        render json: records
     end
+
+    def create 
+        record = Record.new(record_params)
+        if record.save
+            render json: record, status: :accepted
+        else
+            render json: {error: record.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+
+    private
+
+    def record_params
+        params.permit(:record).permit(:date, :time, :blood_pressure, :temperature, :pulse, :pain, :comments, :chart_id)
+    end
+
 end
