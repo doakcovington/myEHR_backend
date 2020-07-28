e26f9a2bb0da9425cde6a81e335a5f05a1c5493b
doakcovington <doakcovington@protonmail.com>
2020-07-28 14:45:42 -0700

    Added dob to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_170554) do
+ActiveRecord::Schema.define(version: 2020_07_28_214303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_07_28_170554) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.date "dob"
   end
 
   create_table "records", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-doak = Chart.create(pcp: "Dr. Hoffman", name: "Doak Covington")
+doak = Chart.create(pcp: "Dr. Hoffman", name: "Doak Covington", dob: "1991-03-09")
 
 day_1 = Record.create(temperature: 98.5, pulse: "78", pain: "2", comments: "feeling good!", chart_id: doak.id)
 day_2 = Record.create(pulse: 82, temperature: 100, pain: "2", comments: "feeling okay", chart_id: doak.id)
