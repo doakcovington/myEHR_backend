4910f7b8ffaac30def5033102c62a4774403b622
doakcovington <doakcovington@protonmail.com>
2020-07-28 10:09:36 -0700

    Added name to chart and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_022753) do
+ActiveRecord::Schema.define(version: 2020_07_28_170554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_022753) do
     t.string "pcp"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "records", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-doak = Chart.create(pcp: "Dr. Hoffman")
+doak = Chart.create(pcp: "Dr. Hoffman", name: "Doak Covington")
 
 day_1 = Record.create(temperature: 98.5, pulse: "78", pain: "2", comments: "feeling good!", chart_id: doak.id)
 day_2 = Record.create(pulse: 82, temperature: 100, pain: "2", comments: "feeling okay", chart_id: doak.id)
