1f316d2fc964af99bb42849d9de4708f5fef4e32
doakcovington <doakcovington@protonmail.com>
2020-07-29 14:24:23 -0700

    Added bp to serializer and strong params

--- app/serializers/record_serializer.rb
+++ app/serializers/record_serializer.rb
@@ -1,4 +1,4 @@
 class RecordSerializer #allows for nested data structure/single fetch calls
   include FastJsonapi::ObjectSerializer
-  attributes :created_at, :temperature, :pulse, :pain, :comments, :chart_id, :chart #(:chart shows the associated chart object)
+  attributes :created_at, :temperature, :pulse, :systolic, :diastolic, :pain, :comments, :chart_id, :chart #(:chart shows the associated chart object)
 end
